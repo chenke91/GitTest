f4bdcd29b1bda6f0a5e7473ae1011c1a9742215a
root <chenke91@qq.com>
2014-04-12 00:48:21 +0800

    the branch version

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ define('APP_DEBUG',true);
 define('APP_NAME','app');
 define('APP_PATH','./app/');
 require('./ThinkPHP/ThinkPHP.php');
-echo 'hello world'
+echo 'hello world';
+echo 'this is the branch';
 ?>
